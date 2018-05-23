07c99e932b5f312fb73cde59cbd24d1eff7903ee
Marcos Neri <marcosneri@outlook.com.br>
2018-05-23 15:11:39 -0300

    Segundo Commit

--- TesteDeEnvio/src/testedeenvio/TesteDeEnvio.java
+++ TesteDeEnvio/src/testedeenvio/TesteDeEnvio.java
@@ -11,9 +11,6 @@ package testedeenvio;
  */
 public class TesteDeEnvio {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
