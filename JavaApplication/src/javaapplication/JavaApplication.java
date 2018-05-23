73c053ff3127af8c2f6063eb994ec5e6afc18f21
Marcos Neri <marcosneri@outlook.com.br>
2018-05-23 14:48:08 -0300

    Teste

--- JavaApplication/src/javaapplication/JavaApplication.java
+++ JavaApplication/src/javaapplication/JavaApplication.java
@@ -11,9 +11,6 @@ package javaapplication;
  */
 public class JavaApplication {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
